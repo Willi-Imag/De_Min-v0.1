d1b24cd30c5f6f659f4bef65af721019353d565c
Willi_Imag <158754613+Willi-Imag@users.noreply.github.com>
2024-11-04 01:51:39 +0700

    Add files via upload

---
+++ main-1.py
@@ -0,0 +1,48 @@
+""" Python3 docu-docu """
+"""
+"""
+import pygame, sys
+import psutil, GPUtil
+"""
+"""
+pygame.init()
+
+_clock = pygame.time.Clock()
+_display = pygame.display
+_screen = _display.set_mode((264, 264), pygame.NOFRAME)
+
+images = [pygame.image.load('images/De_Min_0.png'),
+         pygame.image.load('images/De_Min_1.png'),
+         pygame.image.load('images/De_Min_2.png')]
+
+running = True
+
+while running:
+
+    _screen.fill((149, 99, 149))
+
+    cpu_temp = psutil.sensors_temperatures()['k10temp'][0].current
+
+    if cpu_temp < 50:
+        _screen.blit(images[0], (4, 4))
+    elif cpu_temp < 60:
+        _screen.blit(images[1], (4, 4))
+    elif cpu_temp > 60:
+        _screen.blit(images[2], (4, 4))
+    else: _screen.fill((244, 129, 129))
+    
+    _display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    pygame.time.wait(250)
+    #_clock.tick(1)
+
+pygame.quit()
+sys.exit()
+"""QwQ"""
