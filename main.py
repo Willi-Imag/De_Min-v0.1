4a9756e751d293635ed8515b2b5b1c454f878a4d
Willi_Imag <158754613+Willi-Imag@users.noreply.github.com>
2024-11-03 12:27:19 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import psutil, GPUtil
+"""@~@"""
+cpu_temp = psutil.sensors_temperatures()['k10temp'][0].current
+gpu = GPUtil.getGPUs()[0]
+gpu_temp = gpu.temperature
+"""@~@"""
+print (f"CPU temp: {cpu_temp}")
+print (f"GPU temp: {gpu_temp}")
